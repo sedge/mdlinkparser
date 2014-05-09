37cbbcf9f1ae510741bf069f8f3fcabf7ebf5336
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-05-09 11:33:01 -0400

    MOAH

---
+++ js/sedge_mdlinkparser/README.md
@@ -0,0 +1,12 @@
+mdlinkparser
+============
+
+Let's find those links!
+
+Require, then use:
+
+```javascript
+var linkParser = require("sedge_mdlinkparser");
+
+var links = linkParser.parse( markdownString );
+```
---
+++ js/sedge_mdlinkparser/index.js
@@ -0,0 +1,25 @@
+// Support both requirejs (for front-end) and the nodejs require functionality
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+/**
+* Exports
+*/
+
+define(function( require ) {
+  return function( markdown ) {
+    var mdLink = /\[([^\]]+)\]\(([^)]+)\)/g;
+
+    var links = [];
+    var results;
+
+    if ( !markdown ) {
+      throw "No markdown supplied!";
+    }
+
+    while ( ( (results = mdLink.exec( markdown ) ) !== null ) ) {
+      links.push( results[2] );
+    }
+
+    return links;
+  };
+});
