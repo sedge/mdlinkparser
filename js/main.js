c86706a509cf76e7bd6e6a862ddb120418c3bc46
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-05-09 15:44:27 -0400

    First swing at the front end stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-require.config({
+require( [ "jquery", "sedge_mdlinkparser" ], function( $, mdlinkparser ) {
+  var inputField = $( "input" ),
+      outputField = $( "output" );
 
+  inputField.onblur = function() {
+    outputField.val( mdlinkparser( inputField.val() ));
+  };
 });
